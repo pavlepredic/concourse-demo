1a09484fd9f930de98630b928b6121cbafc37b66
Pavle Predic <pavlepredic@users.noreply.github.com>
2017-09-15 16:09:31 +0200

    Change pipeline

--- ci/scripts/run-php-linter.sh
+++ ci/scripts/run-php-linter.sh
@@ -1,14 +1,14 @@
 #!/usr/bin/env bash
 
-# Ensure this script fails if anything errors
-set -e
-
 # Get the changed files
 CHANGED_FILES=$(git diff --name-only --diff-filter=ACMR origin/master | grep \\.php)
-
-# Only lint when php files changed
-if [ ! -z "$CHANGED_FILES" ]; then
-    # Run PHP linter
-    echo "Checking for syntax errors"
-    echo "$CHANGED_FILES" | xargs -I {} -n1 -P8 php -l {}
+if [ -z "$CHANGED_FILES" ]; then
+    echo "No php files changed; skipping linter."
+    exit 0
 fi
+
+set -e
+
+# Run PHP linter
+echo "Checking for syntax errors"
+echo "$CHANGED_FILES" | xargs -I {} -n1 -P8 php -l {}
