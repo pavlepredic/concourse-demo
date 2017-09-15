13c1066118c59c25b6521f0ef5b6ff08a06f6c05
Pavle Predic <pavle.predic@movingimage.com>
2017-09-15 15:21:31 +0200

    Fix linter

--- ci/scripts/run-php-linter.sh
+++ ci/scripts/run-php-linter.sh
@@ -5,10 +5,10 @@ set -e
 
 # Get the changed files
 CHANGED_FILES=$(git diff --name-only --diff-filter=ACMR origin/master | grep \\.php)
-
-# Only lint when php files changed
-if [ ! -z "$CHANGED_FILES" ]; then
-    # Run PHP linter
-    echo "Checking for syntax errors"
-    echo "$CHANGED_FILES" | xargs -I {} -n1 -P8 php -l {}
+if [ -z "$CHANGED_FILES" ]; then
+    exit 0
 fi
+
+# Run PHP linter
+echo "Checking for syntax errors"
+echo "$CHANGED_FILES" | xargs -I {} -n1 -P8 php -l {}
