e46855bd75a97edc6f3458aa9c400bd13edc7bdd
Pavle Predic <pavle.predic@movingimage.com>
2017-09-21 12:17:32 +0200

    Test

--- ci/scripts/deploy.sh
+++ ci/scripts/deploy.sh
@@ -1,14 +1,24 @@
 #!/usr/bin/env bash
 
+set -e
+
 mkdir ~/.ssh
 echo "$AWS_PRIVATE_KEY" > ~/.ssh/id_rsa
 chmod 600 ~/.ssh/id_rsa
 echo StrictHostKeyChecking=no > ~/.ssh/config
 
 DEPLOYMENT_NAME=concourse-demo
+HAS_PREVIOUS_DEPLOYMENTS=0
 
+set +e
 ssh $AWS_USERNAME@$AWS_IP "ls $DEPLOYMENT_NAME"
 if [ $? == 0 ]; then
+    HAS_PREVIOUS_DEPLOYMENTS=1
+fi
+
+set -e
+
+if [ $HAS_PREVIOUS_DEPLOYMENTS == 1 ]
     echo "Stopping server..."
     ssh $AWS_USERNAME@$AWS_IP "~/$DEPLOYMENT_NAME/bin/stop-server"
 
